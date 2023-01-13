47c919cf1ba8b55d807229bfdee0659c4ba26aa6
Berish Deutsch <berishd8411@gmail.com>
2023-01-13 12:19:06 -0500

    Add project files.

---
+++ ImageSharePassword.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ImageSharePassword.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageSharePassword.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        public IActionResult ViewImage()
+        {
+            return View();
+        }
+    }
+}
